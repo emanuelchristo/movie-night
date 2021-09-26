2baa6063d32b3252b4c714892c1939f81eab1e88
emanuelchristo <emanuelchristochris@gmail.com>
2021-09-26 06:15:03 +0530

    update socket io package

--- plugins/socket-io.js
+++ plugins/socket-io.js
@@ -1,5 +1,5 @@
 const io = require("socket.io-client");
-const socket = io(process.env.SERVER_URL, { withCredentials: false });
+const socket = io(process.env.SERVER_URL);
 
 export default ({ app }, inject) => {
   inject("socket", socket);
